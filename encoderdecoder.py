4b70e07778785f25665c878825cb03f4612632d6
akjayant <ashish.jayant94@gmail.com>
2021-08-31 14:31:34 +0530

    train_file

--- encoderdecoder.py
+++ encoderdecoder.py
@@ -33,6 +33,8 @@ import torch.nn.functional as F
 from collections import OrderedDict
 from tqdm import tqdm
 import pickle
+import sys
+import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 resnet_model = models.resnet18(pretrained=True)
@@ -73,9 +75,118 @@ SOS_token = 0
 EOS_token = 1
 
 
+# class Lang:
+#     def __init__(self, name):
+#         self.name = name
+#         self.word2index = {}
+#         self.word2count = {}
+#         self.index2word = {0: "SOS", 1: "EOS"}
+#         self.n_words = 2  # Count SOS and EOS
+
+#     def addSentence(self, sentence):
+#         for word in sentence.split(' '):
+#             self.addWord(word)
+
+#     def addWord(self, word):
+#         if word not in self.word2index:
+#             self.word2index[word] = self.n_words
+#             self.word2count[word] = 1
+#             self.index2word[self.n_words] = word
+#             self.n_words += 1
+#         else:
+#             self.word2count[word] += 1
+# # Turn a Unicode string to plain ASCII, thanks to
+# # https://stackoverflow.com/a/518232/2809427
+# def unicodeToAscii(s):
+#     return ''.join(
+#         c for c in unicodedata.normalize('NFD', s)
+#         if unicodedata.category(c) != 'Mn'
+#     )
+
+# # Lowercase, trim, and remove non-letter characters
+
+
+# def normalizeString(s):
+#     s = unicodeToAscii(s.lower().strip())
+#     s = re.sub(r"([.!?])", r" \1", s)
+#     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
+#     return s
+
+
+# def readLangs(lang1, lang2, reverse=False):
+#     print("Reading lines...")
+
+#     # Read the file and split into lines
+#     lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
+#         read().strip().split('\n')
+
+#     # Split every line into pairs and normalize
+#     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+
+#     # Reverse pairs, make Lang instances
+#     if reverse:
+#         pairs = [list(reversed(p)) for p in pairs]
+#         input_lang = Lang(lang2)
+#         output_lang = Lang(lang1)
+#     else:
+#         input_lang = Lang(lang1)
+#         output_lang = Lang(lang2)
+
+#     return input_lang, output_lang, pairs
 
 MAX_LENGTH = 25
 
+# eng_prefixes = (
+#     "i am ", "i m ",
+#     "he is", "he s ",
+#     "she is", "she s ",
+#     "you are", "you re ",
+#     "we are", "we re ",
+#     "they are", "they re "
+# )
+
+
+# def filterPair(p):
+#     return len(p[0].split(' ')) < MAX_LENGTH and \
+#         len(p[1].split(' ')) < MAX_LENGTH and \
+#         p[1].startswith(eng_prefixes)
+
+
+# def filterPairs(pairs):
+#     return [pair for pair in pairs if filterPair(pair)]
+
+# def prepareData(lang1, lang2, reverse=False):
+#     input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
+#     print("Read %s sentence pairs" % len(pairs))
+#     pairs = filterPairs(pairs)
+#     print("Trimmed to %s sentence pairs" % len(pairs))
+#     print("Counting words...")
+#     for pair in pairs:
+#         input_lang.addSentence(pair[0])
+#         output_lang.addSentence(pair[1])
+#     print("Counted words:")
+#     print(input_lang.name, input_lang.n_words)
+#     print(output_lang.name, output_lang.n_words)
+#     return input_lang, output_lang, pairs
+
+
+# input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
+# print(random.choice(pairs))
+
+# def indexesFromSentence(lang, sentence):
+#     return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+# def tensorFromSentence(lang, sentence):
+#     indexes = indexesFromSentence(lang, sentence)
+#     indexes.append(EOS_token)
+#     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
+
+
+# def tensorsFromPair(pair):
+#     input_tensor = tensorFromSentence(input_lang, pair[0])
+#     target_tensor = tensorFromSentence(output_lang, pair[1])
+#     return (input_tensor, target_tensor)
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
         super(AttnDecoderRNN, self).__init__()
@@ -219,12 +330,14 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
         input_paths = joined_df['image_seq'][iter-1]
         sos_img = np.zeros([1,512])
         eos_img = np.ones([1,512])
-        input_np=[sos_img]
+        input_np=[]
+        #print(input_paths)
         for i in input_paths.split(','):
+            #print(i)
             img = Image.open(i)
             v = img2vec.get_vec(img,tensor=False).reshape(1,512)
             input_np.append(v)
-        input_np.append(eos_img)
+        
         input_tensor = torch.tensor(input_np).float().to(device)
         #print(input_tensor)
         target_tensor = torch.tensor(joined_df['caption_seq'][iter-1]).to(device)
@@ -235,7 +348,7 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
         plot_loss_total += loss
 
         if iter % print_every == 0:
-            torch.save(encoder1.state_dict(),"encoder_model")
+            torch.save(encoder1.state_dict(),"encoder_model.pkl")
             torch.save(attn_decoder1.state_dict(),"decoder_model.pkl")
             print_loss_avg = print_loss_total / print_every
             print_loss_total = 0
@@ -252,171 +365,231 @@ def trainIters(encoder, decoder, n_iters, print_every=1000, plot_every=100, lear
 
 
 
-flag = sys.argv[1]
-if flag=True:
 
 
 
-	crop_path = '/media/ashish-j/B/wheat_detection/flick_data/detection_data/Images/'
 
-	#---------feature extraction------------------
+crop_path = '/media/ashish-j/B/wheat_detection/flick_data/detection_data/Images/'
 
+#---------feature extraction------------------
+train_flag=sys.argv[1]
+if train_flag=='True':
 
-	image_dict = OrderedDict()
-	files_list = [y for x in os.walk(crop_path) for y in glob(os.path.join(x[0], '*.jpg'))]
-	files_names = [os.path.basename(i) for i in files_list]
-	files_names_set = set(files_names)
+    image_dict = OrderedDict()
+    files_list = [y for x in os.walk(crop_path) for y in glob(os.path.join(x[0], '*.jpg'))]
+    files_names = [os.path.basename(i) for i in files_list]
+    files_names_set = set(files_names)
 
-	c=0
-	for i in range(len(files_list)):
-	    base_name = files_list[i].split("/")[-4]
-	    #print(base_name)
-	    b_idx = base_name
-	    if base_name==files_names[i][:len(base_name)]:
-	        if base_name not in image_dict.keys():
-	            image_dict[b_idx] = str(files_list[i])
-	        else:
-	            image_dict[b_idx] += ","+str(files_list[i])
-
-
-	image_dict_keys = [k for k in image_dict.keys()]
-
-
-	image_df = pd.DataFrame.from_dict(image_dict,orient='index',columns=['image_seq'])
-	image_df['image'] = image_df.index
-	idx =  [i for i in range(image_df.shape[0])]
-	#print(image_df.head())
+    c=0
+    original_file_path = '/media/ashish-j/B/wheat_detection/flick_data/Images/'
+    for i in range(len(files_list)):
+        base_name = files_list[i].split("/")[-4]
+        b_idx = base_name
+        orig_file = os.path.join(original_file_path,base_name)
+        image_dict[base_name] = orig_file
+        if base_name==files_names[i][:len(base_name)]:
+            if base_name not in image_dict.keys():
+                image_dict[base_name] = str(files_list[i])
+                #image_dict[base_name] += ","+str(orig_file)    
+            else:    
+                image_dict[b_idx] += ","+str(files_list[i])
+        #print(image_dict[b_idx])
 
 
-	embedding_input_size = len(files_list)
-	def first_caption(x):
-	    return x[0]
-	df = pd.read_csv("captions.txt",names=['image','caption'],sep=',')
-	df = df.groupby("image")['caption'].apply(list).reset_index(name='caption')
-	df['caption']=df['caption'].apply(first_caption)
-	#print(df.shape)
-	#print(df.head())
-
-	joined_df = pd.merge(df,image_df,on='image')
-	#joined_df.to_csv("data.csv")
-
-	# print("training pairs")
-	# n_iters = 50
-	# training_pairs = [tensorsFromPair(random.choice(pairs))
-	#                       for i in range(n_iters)]
-	# print(training_pairs[0])
-
-	words = []
-	for idx, row in joined_df.iterrows():
-	    #title
-	    words.extend(joined_df['caption'][idx].split(" "))
-	    #paragraph context
-
-	words = set(words)
-	words = list(words)
-	n_words = len(words)+2
-	                                                                                     
-	words_idx = {'<SOS>':0,
-	'<EOS>':1}
-	for i in range(len(words)):
-	    words_idx[words[i]]= i+2
+    image_dict_keys = [k for k in image_dict.keys()]
 
 
+    image_df = pd.DataFrame.from_dict(image_dict,orient='index',columns=['image_seq'])
+    image_df['image'] = image_df.index
+    
 
-	
+    embedding_input_size = len(files_list)
+    def first_caption(x):
+        return x[0]
+    df = pd.read_csv("captions.txt",names=['image','caption'],sep=',',skiprows=1)
+    #df = df.groupby("image")['caption'].apply(list).reset_index(name='caption')
+    #df['caption']=df['caption'].apply(first_caption)
+    print(df.shape)
+    print(df.head())
+    print(image_df.head())
+    joined_df = pd.merge(df,image_df,on='image')
+    #joined_df.to_csv("data.csv")
+
+    # print("training pairs")
+    # n_iters = 50
+    # training_pairs = [tensorsFromPair(random.choice(pairs))
+    #                       for i in range(n_iters)]
+    # print(training_pairs[0])
+
+    words = []
+    for idx, row in joined_df.iterrows():
+        words.extend(joined_df['caption'][idx].lower().split(" "))
+        #paragraph context
+
+    words = set(words)
+    words = list(words)
+    n_words = len(words)+2
+
+    words_idx = {}                                                                                
+    words_idx['<SOS>']=0
+    words_idx['<EOS>']=1
+    for i in range(len(words)):
+        words_idx[words[i]]= i+2
+
+    #print(words_idx)
+
     a_file = open("word_dict.pkl", "wb")
-    pickle. dump(words_idx, a_file)
-
-	def tokenizer(x):
-	    embed = []
-	    embed.append([0])
-	    for i in x.split(" "):
-	        try:
-	            embed.append([words_idx[i]])
-	        except:
-	            pass
-	    embed.append([1])
-	    return embed
-	joined_df['caption_seq']=joined_df['caption'].apply(tokenizer)
-
-	print(joined_df.head())
-	print(joined_df['caption_seq'][0])
+    pickle.dump(words_idx, a_file)
 
 
+    def tokenizer(x):
+        embed = [[0]]
+        
+        for i in x.lower().split(" "):
+          
+            embed.append([words_idx[i]])
+            
+        embed.append([1])
+        return embed
+    joined_df['caption_seq']=joined_df['caption'].apply(tokenizer)
+    def randomizer(x):
+        l = x.split(',')
 
-	hidden_size = 512
-	encoder1 = EncoderRNN(embedding_input_size, hidden_size).to(device)
-	attn_decoder1 = AttnDecoderRNN(hidden_size, n_words, dropout_p=0.1).to(device)
-	trainIters(encoder1, attn_decoder1, 7995, print_every=500)
-	torch.save(encoder1.state_dict(),"encoder_model")
-	torch.save(attn_decoder1.state_dict(),"decoder_model.pkl")
-	joined_df.to_csv("last_train_data.csv",sep='\t')
+        print('bs',l)
+        random.shuffle(l)
+        print('as',l)
+        return l
+    #joined_df['image_seq']=joined_df['image_seq'].apply(randomizer)
 
+
+    print(joined_df.head())
+    print(joined_df.shape)
+    print(n_words,embedding_input_size)
+    joined_df.to_csv("last_train_data.csv",sep='\t')
+
+
+    hidden_size = 512
+    encoder1 = EncoderRNN(embedding_input_size, hidden_size).to(device)
+    attn_decoder1 = AttnDecoderRNN(hidden_size, n_words, dropout_p=0.1).to(device)
+    trainIters(encoder1, attn_decoder1, 39900, print_every=1000)
+    torch.save(encoder1.state_dict(),"encoder_model.pkl")
+    torch.save(attn_decoder1.state_dict(),"decoder_model.pkl")
 
 else:
-	a_file = open("word_dict.pkl", "rb")
-	words_idx = pickle.load(a_file)
+    import ast
+    from nltk.translate.bleu_score import sentence_bleu, SmoothingFunction
+    smoothie = SmoothingFunction().method4
+    a_file = open("word_dict.pkl", "rb")
+    words_idx = pickle.load(a_file)
 
-	max_length=25
-	def eval(jdf,encoder,decoder,i):
-	    input_paths = joined_df['image_seq'][i]
-	    sos_img = np.zeros([1,512])
-	    eos_img = np.ones([1,512])
-	    input_np=[sos_img]
-	    for i in input_paths.split(','):
-	        img = Image.open(i)
-	        v = img2vec.get_vec(img,tensor=False).reshape(1,512)
-	        input_np.append(v)
-	    input_np.append(eos_img)
-	    
-	    input_tensor = torch.tensor(input_np).float().to(device)
-	    input_length = input_tensor.size()[0]
-	    encoder_hidden = encoder.initHidden()
+    max_length=25
+    hidden_size = 512
+    n_words=8873
+    embedding_input_size=42446
+    encoder1 = EncoderRNN(embedding_input_size, hidden_size).to(device)
+    attn_decoder1 = AttnDecoderRNN(hidden_size, n_words, dropout_p=0.1).to(device)
+    def eval(jdf,encoder,decoder,i):
+        input_paths = jdf['image_seq'][i]
+        sos_img = np.zeros([1,512])
+        eos_img = np.ones([1,512])
+        
+        input_np =[]
+        #input_paths = ast.literal_eval(input_paths)
+        for i in input_paths.split(","):
+           
+            img = Image.open(i)
+            v = img2vec.get_vec(img,tensor=False).reshape(1,512)
+            input_np.append(v)
+        
+        
+        input_tensor = torch.tensor(input_np).float().to(device)
+        input_length = input_tensor.size()[0]
+        encoder_hidden = encoder.initHidden()
 
-	    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+        encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
 
-	    for ei in range(input_length):
-	        encoder_output, encoder_hidden = encoder(input_tensor[ei],
-	                                                 encoder_hidden)
-	        encoder_outputs[ei] += encoder_output[0, 0]
+        for ei in range(input_length):
+            encoder_output, encoder_hidden = encoder(input_tensor[ei],
+                                                     encoder_hidden)
+            encoder_outputs[ei] += encoder_output[0, 0]
 
-	    decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
+        decoder_input = torch.tensor([[SOS_token]], device=device)  # SOS
 
-	    decoder_hidden = encoder_hidden
+        decoder_hidden = encoder_hidden
 
-	    decoded_words = []
-	    decoder_attentions = torch.zeros(max_length, max_length)
-	    inv = {}
-	    for key, val in words_idx.items():
-	        inv[val] = inv.get(val, []) + [key]
-	    for di in range(max_length):
-	        decoder_output, decoder_hidden, decoder_attention = decoder(
-	            decoder_input, decoder_hidden, encoder_outputs)
-	        #decoder_attentions[di] = decoder_attention.data
-	        topv, topi = decoder_output.data.topk(1)
-	        if topi.item() == EOS_token:
-	            decoded_words.append('<EOS>')
-	            break
-	        else:
-	            
-	            decoded_words.append(inv[topi.item()])
+        decoded_words = []
+        decoder_attentions = torch.zeros(max_length, max_length)
+        inv = {}
+        for key, val in words_idx.items():
+            inv[val] = inv.get(val, []) + [key]
+        for di in range(max_length):
+            decoder_output, decoder_hidden, decoder_attention = decoder(
+                decoder_input, decoder_hidden, encoder_outputs)
+            #decoder_attentions[di] = decoder_attention.data
+            topv, topi = decoder_output.data.topk(1)
+            if topi.item() == EOS_token:
+                decoded_words.append(['<EOS>'])
+                break
+            else:
+                
+                decoded_words.append(inv[topi.item()])
 
-	        decoder_input = topi.squeeze().detach()
+            decoder_input = topi.squeeze().detach()
 
-	    return decoded_words
+        return decoded_words
 
 
-	ld = pd.read_csv("last_train_data.csv",sep='\t')
-	ec = torch.load("encoder_model")
-	encoder1.load_state_dict(ec)
-	dc = torch.load("decoder_model.pkl")
-	attn_decoder1.load_state_dict(dc)
-	#print(joined_df.head(50))
-	print("prediction-------------------------------------------------")
-	for i in [1,2,3,4,5,6,7,8,9,10]:
-	    print("correct",joined_df['caption'][i])
-	    print("prediction-",eval(ld,encoder1,attn_decoder1,i))
-	    print("-------")
+    ld = pd.read_csv("last_train_data.csv",sep='\t')
+    ec = torch.load("encoder_model.pkl")
+    encoder1.load_state_dict(ec)
+    dc = torch.load("decoder_model.pkl")
+    attn_decoder1.load_state_dict(dc)
+    #print(joined_df.head(50))
+    preds = {}
+    refs = {}
+    print("Prediction-------------------------------------------------")
+    for i in range(1,10):
+        print("correct",ld['caption'][i])
+        print("prediction-",eval(ld,encoder1,attn_decoder1,i))
+        print("-------")
+
+    print("---------------VALIDATION BLEU SCORE----------------------------------- ")
+    reference_df = pd.read_csv("captions.txt",names=['image','caption'],sep=',',skiprows=1)
+    reference_df = reference_df[39900:]
+    reference_df = reference_df.groupby("image")['caption'].apply(list).reset_index(name='caption')
+    #print(reference_df.head())
+    for i in range(reference_df.shape[0]):
+        refs[reference_df['image'][i]]=(reference_df['caption'][i],i)
+    
+    #print(refs)
+
+    mean_bleu_score = 0
+    n=0
+    for i in refs.keys():
+        true,k = refs[i]
+        trues=[]
+        for j in true:
+            trues.append(j.split(" "))
+        
+        hypothesis = eval(ld,encoder1,attn_decoder1,k)
+        try:
+            hypothesis.remove(['<SOS>'])
+            hypothesis.remove(['<EOS>'])
+        except:
+            pass
+        preds = []
+        for l in hypothesis:
+            preds.append(l[0])
+       
+        mean_bleu_score+=sentence_bleu(trues,preds,smoothing_function=smoothie)
+        n+=1
+
+    print("Mean Bleu Score=",mean_bleu_score/n)
+
+
+    
+    
+
 
 
 
