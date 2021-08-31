bcf6235f3ee662575ee0dd8b4d3e3e7030dbb293
akjayant <ashish.jayant94@gmail.com>
2021-08-31 14:32:43 +0530

    train_file

--- encoderdecoder.py
+++ encoderdecoder.py
@@ -57,7 +57,7 @@ class EncoderRNN(nn.Module):
         super(EncoderRNN, self).__init__()
         self.hidden_size = hidden_size
 
-        self.embedding = nn.Embedding(input_size, hidden_size)
+        #self.embedding = nn.Embedding(input_size, hidden_size)
         self.gru = nn.GRU(hidden_size, hidden_size)
 
     def forward(self, input, hidden):
@@ -75,118 +75,6 @@ SOS_token = 0
 EOS_token = 1
 
 
-# class Lang:
-#     def __init__(self, name):
-#         self.name = name
-#         self.word2index = {}
-#         self.word2count = {}
-#         self.index2word = {0: "SOS", 1: "EOS"}
-#         self.n_words = 2  # Count SOS and EOS
-
-#     def addSentence(self, sentence):
-#         for word in sentence.split(' '):
-#             self.addWord(word)
-
-#     def addWord(self, word):
-#         if word not in self.word2index:
-#             self.word2index[word] = self.n_words
-#             self.word2count[word] = 1
-#             self.index2word[self.n_words] = word
-#             self.n_words += 1
-#         else:
-#             self.word2count[word] += 1
-# # Turn a Unicode string to plain ASCII, thanks to
-# # https://stackoverflow.com/a/518232/2809427
-# def unicodeToAscii(s):
-#     return ''.join(
-#         c for c in unicodedata.normalize('NFD', s)
-#         if unicodedata.category(c) != 'Mn'
-#     )
-
-# # Lowercase, trim, and remove non-letter characters
-
-
-# def normalizeString(s):
-#     s = unicodeToAscii(s.lower().strip())
-#     s = re.sub(r"([.!?])", r" \1", s)
-#     s = re.sub(r"[^a-zA-Z.!?]+", r" ", s)
-#     return s
-
-
-# def readLangs(lang1, lang2, reverse=False):
-#     print("Reading lines...")
-
-#     # Read the file and split into lines
-#     lines = open('data/%s-%s.txt' % (lang1, lang2), encoding='utf-8').\
-#         read().strip().split('\n')
-
-#     # Split every line into pairs and normalize
-#     pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
-
-#     # Reverse pairs, make Lang instances
-#     if reverse:
-#         pairs = [list(reversed(p)) for p in pairs]
-#         input_lang = Lang(lang2)
-#         output_lang = Lang(lang1)
-#     else:
-#         input_lang = Lang(lang1)
-#         output_lang = Lang(lang2)
-
-#     return input_lang, output_lang, pairs
-
-MAX_LENGTH = 25
-
-# eng_prefixes = (
-#     "i am ", "i m ",
-#     "he is", "he s ",
-#     "she is", "she s ",
-#     "you are", "you re ",
-#     "we are", "we re ",
-#     "they are", "they re "
-# )
-
-
-# def filterPair(p):
-#     return len(p[0].split(' ')) < MAX_LENGTH and \
-#         len(p[1].split(' ')) < MAX_LENGTH and \
-#         p[1].startswith(eng_prefixes)
-
-
-# def filterPairs(pairs):
-#     return [pair for pair in pairs if filterPair(pair)]
-
-# def prepareData(lang1, lang2, reverse=False):
-#     input_lang, output_lang, pairs = readLangs(lang1, lang2, reverse)
-#     print("Read %s sentence pairs" % len(pairs))
-#     pairs = filterPairs(pairs)
-#     print("Trimmed to %s sentence pairs" % len(pairs))
-#     print("Counting words...")
-#     for pair in pairs:
-#         input_lang.addSentence(pair[0])
-#         output_lang.addSentence(pair[1])
-#     print("Counted words:")
-#     print(input_lang.name, input_lang.n_words)
-#     print(output_lang.name, output_lang.n_words)
-#     return input_lang, output_lang, pairs
-
-
-# input_lang, output_lang, pairs = prepareData('eng', 'fra', True)
-# print(random.choice(pairs))
-
-# def indexesFromSentence(lang, sentence):
-#     return [lang.word2index[word] for word in sentence.split(' ')]
-
-
-# def tensorFromSentence(lang, sentence):
-#     indexes = indexesFromSentence(lang, sentence)
-#     indexes.append(EOS_token)
-#     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
-
-
-# def tensorsFromPair(pair):
-#     input_tensor = tensorFromSentence(input_lang, pair[0])
-#     target_tensor = tensorFromSentence(output_lang, pair[1])
-#     return (input_tensor, target_tensor)
 class AttnDecoderRNN(nn.Module):
     def __init__(self, hidden_size, output_size, dropout_p=0.1, max_length=MAX_LENGTH):
         super(AttnDecoderRNN, self).__init__()
